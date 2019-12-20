dc5b36a087430eb006e200bface437ce2722ecd6
BiggieBoo18 <yahoomaildb@gmail.com>
2019-12-20 14:14:14 +0900

    idft

--- src/main.rs
+++ src/main.rs
@@ -51,8 +51,29 @@ fn dft<I>(data: &mut I, start: usize, N: usize) -> Option<Vec<Complex<f64>>>
             ret[start+k] += Complex::new(rk, ik);
             // println!("rk={}", rk);
             // println!("ik={}", ik);
-            // println!("ret={}", ret[start+k]);
         }
+        // println!("ret={}", ret[start+k]);
+    }
+    Some(ret)
+}
+
+fn idft(data: Vec<Complex<f64>>, N: usize) -> Option<Vec<Complex<f64>>>
+    {
+    let mut ret: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0);N];
+    for k in 0..N {
+        let w  = 2.0*std::f64::consts::PI/N as f64;
+        for n in 0..N {
+            let rk = data[n].re;
+            let ik = data[n].im;
+            // println!("k={}, n={}, rk={}, ik={}", k, n, rk, ik);
+            let ph = w * k as f64 * n as f64;
+            let r_f = rk*(ph.cos())-ik*(ph.sin());
+            let i_f = ik*(ph.sin())+ik*(ph.cos());
+            ret[k] += Complex::new(r_f/(N as f64), i_f/(N as f64));
+            // println!("r_f={}", r_f);
+            // println!("i_f={}", i_f);
+        }
+        // println!("ret={}", ret[k]);
     }
     Some(ret)
 }
@@ -60,10 +81,12 @@ fn dft<I>(data: &mut I, start: usize, N: usize) -> Option<Vec<Complex<f64>>>
 use std::io::Write;
 fn main() {
     // create sine wave
-    let source = create_sine_wave(1.0, 256.0, 44100.0, 1.0);
+    let source = create_sine_wave(1.0, 440.0, 44100.0, 1.0);
     // println!("{}", source.sample_rate()); // 44100
     // println!("{}", source.channels()); // 1
+    let fs = source.sample_rate();
     let N = 4096;
+    // dft
     let dft_data = match dft(&mut source.clone(), 0, N) {
         Some(data) => data,
         None => {
@@ -71,14 +94,33 @@ fn main() {
             std::process::exit(1);
         }
     };
-    let fs = source.sample_rate();
+
+    // idft
+    let idft_data = match idft(dft_data.clone(), N) {
+        Some(data) => data,
+        None => {
+            writeln!(std::io::stderr(), "IDFT Error!").unwrap();
+            std::process::exit(1);
+        }
+    };
+    let vec_idft_data: Vec<i16> = idft_data.iter().map(|d| (d.re*32768.0) as i16).collect();
+    let idft_source = SamplesBuffer::new(1, fs as u32, vec_idft_data).buffered();
+
+
     let freq_vec: Vec<f64> = (0..N).map(|k| k as f64*fs as f64/N as f64).collect();
     // draw graph
-    let data: Vec<(f64, f64)> = source.clone().take(255).enumerate().map(|(i, x)| (i as f64, x as f64)).collect();
+    // let data: Vec<(f64, f64)> = source.clone().take(255).enumerate().map(|(i, x)| (i as f64, x as f64/32767.0)).collect();
+    let data: Vec<(f64, f64)> = idft_source.clone().take(440).enumerate().map(|(i, x)| (i as f64, x as f64/32767.0)).collect();
     draw_graph(&data, "wave.svg").expect("Failed draw graph");
-    let amplitude: Vec<(f64, f64)> = dft_data.clone().iter().map(|d| d.norm_sqr()).zip(freq_vec.clone()).map(|(a, b)| (b, a)).collect();
+    let amplitude: Vec<(f64, f64)> = dft_data.clone().iter().map(|d| d.norm_sqr().sqrt()).zip(freq_vec.clone()).map(|(a, b)| (b, a)).collect();
     draw_graph(&amplitude, "amp.svg").expect("Failed draw graph");
-    let phase: Vec<(f64, f64)> = dft_data.clone().iter().map(|d| d.im.round().atan2(d.re.round())).zip(freq_vec.clone()).map(|(a, b)| (b, a)).collect();
+    let phase: Vec<(f64, f64)> = dft_data.clone().iter().map(|d| {
+        let mut ret = d.im.round().atan2(d.re.round());
+        if ret.abs()>=std::f64::consts::PI/2.0 {
+            ret = 0.0;
+        }
+        ret
+    }).zip(freq_vec.clone()).map(|(a, b)| (b, a)).collect();
     draw_graph(&phase, "phase.svg").expect("Failed draw graph");
 
     // println!("{:?}", source.max());
